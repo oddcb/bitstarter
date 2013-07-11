677481ab5143d0a34e55823cfcc069a4c30eb11a
oddcb <oddchrister@gmail.com>
2013-07-11 20:55:05 +0200

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readIndexToBuffer().toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var readIndexToBuffer = function() {
+    return fs.readFileSync("index.html")
+}
